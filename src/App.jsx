cbd56b49d5ec99347b60dbd8f0ed1a202d9009aa
Alex Perkowski <perkowskialex@gmail.com>
2020-01-14 14:13:03 -0600

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import Button from './components/Button'
-import './App.css'
-import { store, cart } from './data'
-import Store from './views/Store'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import Button from "./components/Button";
+import "./App.css";
+import { store, cart } from "./data";
+import Store from "./views/Store";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 class App extends Component {
-	state = {
-		storeData: [],
-		cartData: []
-	}
+  state = {
+    storeData: [],
+    cartData: []
+  };
 
-	componentDidMount() {
-		this.setState({
-			storeData: store,
-			cartData: cart
-		})
-	}
+  componentDidMount() {
+    this.setState({
+      storeData: store,
+      cartData: cart
+    });
+  }
 
-	render() {
-		return (
-			<div className="App">
-				<Store store={this.state.storeData} />
-			</div>
-		)
-	}
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <Store store={this.state.storeData} />}
+          />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;

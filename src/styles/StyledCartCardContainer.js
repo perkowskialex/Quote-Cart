feede266b22cc7335acd87972c9bd27add875ffc
Andrew Murray <darbyguitars@gmail.com>
2020-01-14 14:02:24 -0600

    add a styled component to store.js
    	modified:   package.json
    	new file:   src/styles/StyledCartCardContainer.js
    	modified:   src/views/Store.js
    	modified:   yarn.lock

---
+++ src/styles/StyledCartCardContainer.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const StyledStoreCardContainer = styled.div`
+margin: 8vh;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: space-around;
+	.store-card {
+		margin-bottom: 6vh;
+	}
+	.card-action-container {
+		display: flex;
+		flex-direction: row-reverse;
+	}
+	.add-button {
+		height: 12vh;
+		width: 12vh;
+		position: fixed;
+		bottom: 2%;
+		right: 8%;
+		z-index: 1;
+		background-color: #14a37f;
+	}
+`
--- src/views/Store.js
+++ src/views/Store.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import StoreCard from '../components/StoreCard'
+import { StyledStoreCardContainer } from '../styles/StyledCartCardContainer'
 
 function Store({ store }) {
 	const storeCards = store.map(
 		({ id, author, image, quantity, price, quote }) => (
-            <StoreCard 
-                id={id}
-                author={author}
-                image={image}
-                quantity={quantity}
-                price={price}
-                quote={quote}
-            /> 
+			<StoreCard
+				id={id}
+				author={author}
+				image={image}
+				quantity={quantity}
+				price={price}
+				quote={quote}
+			/>
 		)
 	)
-	return storeCards
+	return <StyledStoreCardContainer>{storeCards}</StyledStoreCardContainer>
 }
 
 export default Store
